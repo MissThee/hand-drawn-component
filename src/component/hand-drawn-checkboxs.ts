07fe73770669ac1553edd2eb40c8826782074c61
MT <1163182715@qq.com>
2021-06-26 21:52:55 +0800

    add checkboxs

---
+++ src/component/hand-drawn-checkboxs.ts
@@ -0,0 +1,60 @@
+import {html, PropertyValues} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {HandDrawnBase} from './base/hand-drawn-base';
+import './hand-drawn-checkbox'
+
+export interface CheckBoxItem {
+  value: string
+  name: string
+  disabled: boolean
+  checked: boolean
+}
+
+@customElement('hand-drawn-checkboxs')
+export class HandDrawnCheckboxs extends HandDrawnBase {
+  @property({type: Boolean}) disabled = false;
+  @property({type: String}) value: string | null = null;
+  @property({type: Array}) data: CheckBoxItem[] = [];
+  @property({type: Array, reflect: true}) checkedItems: CheckBoxItem[] = [];
+
+
+  protected render() {
+    return html`
+      ${this.data.map(item =>
+          html`
+            <hand-drawn-checkbox
+              @change="${this.change}"
+              renderType="${this.renderType}"
+              value="${item.value}"
+              ?checked="${item.checked}"
+              ?disabled="${this.disabled || item.disabled}">${item.name}
+            </hand-drawn-checkbox>
+          `
+      )}
+    `;
+  }
+
+  protected firstUpdated(_changedProperties: PropertyValues) {
+    super.firstUpdated(_changedProperties);
+    this.checkedItems = this.data?.filter(e => e.checked) || []
+    // console.log('firstUpdated', this.checkedItems)
+  }
+
+  private change(e: CustomEvent) {
+    // console.log(e);
+    this.data.forEach(item => {
+      if (e.detail.value === item.value) {
+        item.checked = e.detail.checked
+      }
+    })
+    this.checkedItems = this.data?.filter(e => e.checked) || []
+    // console.log('change', this.checkedItems)
+  }
+
+  static get styles() {
+    return [
+      super.styles,
+    ];
+  }
+
+}
