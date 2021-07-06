c2a13973b31a6d0479f3a712459db1a73f55e85d
MissThee <1163182715@qq.com>
2021-07-06 12:54:09 +0800

    add switch preview

---
+++ src/component/hand-drawn-switch.ts
@@ -0,0 +1,140 @@
+import {css, html} from 'lit';
+import {customElement, property, query} from 'lit/decorators.js';
+import {HandDrawnBase, RoughObjCanvas, RoughObjSvg} from './base/hand-drawn-base';
+
+@customElement('hand-drawn-switch')
+export class HandDrawnSwitch extends HandDrawnBase {
+  @property({type: Boolean, reflect: true}) disabled = false;
+  @property({type: Boolean}) checked = false;
+  @query('input') private input?: HTMLInputElement;
+
+  protected render() {
+    return html`
+        <label class="switch" ?disabled="${this.disabled}">
+            <input class="switch-input" @change="${this.checkSwitchHandler}" type="checkbox" ?disabled="${this.disabled}" .checked="${this.checked}">
+            <div id="switchWrapper" class="switch-wrapper rough ${this.checked ? 'switch-wrapper--active' : ''}">
+                <div id="switchToggle" class="switch-toggle rough ${this.checked ? 'switch-toggle--active' : ''}"></div>
+            </div>
+        </label>
+    `;
+  }
+
+  private checkSwitchHandler() {
+    this.checked = this.input!.checked;
+    console.log(this.checked);
+    this.dispatchEvent(new CustomEvent('change', {
+      composed: true,
+      bubbles: true,
+      detail: {
+        checked: this.checked
+      }
+    }));
+  }
+
+  protected updateAnimationState() {
+    if (!this.disabled) {
+      super.updateAnimationState();
+    }
+  }
+
+  protected roughDrawOne(roughObj: RoughObjSvg | RoughObjCanvas) {
+    const size = {
+      width: roughObj.roughParentEl.clientWidth,
+      height: roughObj.roughParentEl.clientHeight
+    };
+    if (roughObj.roughParentEl.id === 'switchWrapper') {
+      if (roughObj.roughEl instanceof HTMLCanvasElement) {
+        roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
+      }
+      const nodeArray = [];
+      nodeArray.push(roughObj.roughInstance.line(size.height / 5 * 2 + this.roughPadding, this.roughPadding, size.width - (size.height / 5 * 2 + this.roughPadding), this.roughPadding, this.roughOps));
+      nodeArray.push(roughObj.roughInstance.line(size.height / 5 * 2 + this.roughPadding, size.height - this.roughPadding, size.width - (size.height / 5 * 2 + this.roughPadding), size.height - this.roughPadding, this.roughOps));
+      nodeArray.push(roughObj.roughInstance.arc(size.height / 5 * 2 + this.roughPadding, size.height / 2, size.height / 5 * 4 - this.roughPadding * 2, size.height / 5 * 4, Math.PI / 2, Math.PI / 2 * 3, false, this.roughOps));
+      nodeArray.push(roughObj.roughInstance.arc(size.width - (size.height / 5 * 2 + this.roughPadding), size.height / 2, size.height / 5 * 4 , size.height - this.roughPadding*2, Math.PI / 2 * 3, Math.PI / 2 * 5, false, this.roughOps));
+      if (roughObj.roughEl instanceof SVGSVGElement) {
+        roughObj.roughEl.innerHTML = '';
+        for (let node of nodeArray) {
+          roughObj.roughEl.appendChild(<Node>node);
+        }
+      }
+    } else if (roughObj.roughParentEl.id === 'switchToggle') {
+      if (roughObj.roughEl instanceof HTMLCanvasElement) {
+        roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
+      }
+      const nodeArray = [];
+      nodeArray.push(roughObj.roughInstance.circle(size.height / 5 * 2 + this.roughPadding, size.height / 2, size.height / 5 * 4, {fill: this.roughOps.stroke, fillStyle: 'zigzag', ...this.roughOps, strokeWidth: 1}));
+      if (roughObj.roughEl instanceof SVGSVGElement) {
+        roughObj.roughEl.innerHTML = '';
+        for (let node of nodeArray) {
+          roughObj.roughEl.appendChild(<Node>node);
+        }
+      }
+    } else {
+      super.roughDrawOne(roughObj);
+    }
+  }
+
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        .slot {
+          display: inline-block;
+          vertical-align: middle;
+        }
+
+        .switch {
+          overflow: hidden;
+          position: relative;
+          user-select: none;
+          border: none;
+          background: none;
+          cursor: pointer;
+          outline: none;
+          height: 100%;
+        }
+
+        .switch-input {
+          width: 0;
+          height: 0;
+          opacity: 1;
+          position: absolute;
+        }
+
+        .switch-wrapper {
+          display: inline-block;
+          overflow: hidden;
+          position: relative;
+          user-select: none;
+          border: none;
+          background: none;
+          outline: none;
+          vertical-align: middle;
+          width: 3.5em;
+          height: 1.5em;
+        }
+
+        .switch-toggle {
+          height: 1.5em;
+          width: 1.5em;
+          position: absolute;
+          top: 0;
+          transition: all 0.2s ease-out;
+        }
+
+        .switch-toggle--active {
+          margin: 0 0 0 100%;
+          transform: translateX(-100%);
+        }
+
+        .switch[disabled] {
+          opacity: 0.5;
+          background: rgba(0, 0, 0, 0.03);
+          cursor: not-allowed;
+        }
+      `
+    ];
+  }
+
+
+}
