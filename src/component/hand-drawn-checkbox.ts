3d4bce11b7eec85cb074c373ef06c537d8060a0e
MT <1163182715@qq.com>
2021-07-10 19:42:14 +0800

    update style of checkbox, radio

--- src/component/hand-drawn-checkbox.ts
+++ src/component/hand-drawn-checkbox.ts
@@ -4,120 +4,116 @@ import {HandDrawnBase, RoughObjCanvas, RoughObjSvg} from './base/hand-drawn-base
 
 @customElement('hand-drawn-checkbox')
 export class HandDrawnCheckbox extends HandDrawnBase {
-  @property({type: Boolean, reflect: true}) disabled = false;
-  @property({type: Boolean}) checked = false;
-  @property({type: String}) value: string | null = null;
-  @query('input') private input?: HTMLInputElement;
+    @property({type: Boolean, reflect: true}) disabled = false;
+    @property({type: Boolean}) checked = false;
+    @property({type: String}) value: string | null = null;
+    @query('input') private input?: HTMLInputElement;
 
-  protected render() {
-    return html`
-        <label class="checkbox" ?disabled="${this.disabled}">
-            <input class="checkbox-input" @change="${this.checkSwitchHandler}" type="checkbox" ?disabled="${this.disabled}" .checked="${this.checked}" value="${this.value}">
-            <div class="checkbox-wrapper rough">
-                <div id="tick" style=${this.checked ? 'display:inline-block' : 'display:none'} class="checkbox-tick rough"></div>
-            </div>
-            <slot class="slot" @slotchange="${this.roughRender}"></slot>
-        </label>
-    `;
-  }
-
-  private checkSwitchHandler() {
-    this.checked = this.input!.checked;
-    this.dispatchEvent(new CustomEvent('change', {
-      composed: true,
-      bubbles: true,
-      detail: {
-        value: this.value,
-        checked: this.checked
-      }
-    }));
-  }
-
-  protected updateAnimationState() {
-    if (!this.disabled) {
-      super.updateAnimationState();
+    protected render() {
+        return html`
+            <label class="checkbox" ?disabled="${this.disabled}">
+                <input class="checkbox-input" @change="${this.checkSwitchHandler}" type="checkbox" ?disabled="${this.disabled}" .checked="${this.checked}" value="${this.value}">
+                <div class="checkbox-wrapper rough">
+                    <div id="tick" style=${this.checked ? 'display:inline-block' : 'display:none'} class="checkbox-tick rough"></div>
+                </div><!--
+             --><slot class="slot" @slotchange="${this.roughRender}"></slot>
+            </label>
+        `;
     }
-  }
 
-  protected roughDrawOne(roughObj: RoughObjSvg | RoughObjCanvas) {
-    const size = {
-      width: roughObj.roughParentEl.clientWidth,
-      height: roughObj.roughParentEl.clientHeight
-    };
-    if (roughObj.roughParentEl.id === 'tick') {
-      if (roughObj.roughEl instanceof HTMLCanvasElement) {
-        roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
-      }
-      const nodeArray = [];
-      nodeArray.push(roughObj.roughInstance.line(size.width / 5, size.height / 3, size.width / 5 * 2, size.height / 5 * 4, this.roughOps));
-      nodeArray.push(roughObj.roughInstance.line(size.width / 5 * 2, size.height / 5 * 4, size.width, 0, this.roughOps));
-      if (roughObj.roughEl instanceof SVGSVGElement) {
-        roughObj.roughEl.innerHTML = '';
-        for (let node of nodeArray) {
-          roughObj.roughEl.appendChild(<Node>node);
-        }
-      }
-    } else {
-      super.roughDrawOne(roughObj);
+    private checkSwitchHandler() {
+        this.checked = this.input!.checked;
+        this.dispatchEvent(new CustomEvent('change', {
+            composed: true,
+            bubbles: true,
+            detail: {
+                value: this.value,
+                checked: this.checked
+            }
+        }));
     }
-  }
 
-  static get styles() {
-    return [
-      super.styles,
-      css`
-        .slot {
-          float: left;
-          display: inline-block;
-          vertical-align: middle;
+    protected updateAnimationState() {
+        if (!this.disabled) {
+            super.updateAnimationState();
         }
+    }
 
-        .checkbox {
-          overflow: hidden;
-          position: relative;
-          border: none;
-          background: none;
-          cursor: pointer;
-          outline: none;
-          height: 100%;
+    protected roughDrawOne(roughObj: RoughObjSvg | RoughObjCanvas) {
+        const size = {
+            width: roughObj.roughParentEl.clientWidth,
+            height: roughObj.roughParentEl.clientHeight
+        };
+        if (roughObj.roughParentEl.id === 'tick') {
+            if (roughObj.roughEl instanceof HTMLCanvasElement) {
+                roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
+            }
+            const nodeArray = [];
+            nodeArray.push(roughObj.roughInstance.line(size.width / 5, size.height / 3, size.width / 5 * 2, size.height / 5 * 4, this.roughOps));
+            nodeArray.push(roughObj.roughInstance.line(size.width / 5 * 2, size.height / 5 * 4, size.width, 0, this.roughOps));
+            if (roughObj.roughEl instanceof SVGSVGElement) {
+                roughObj.roughEl.innerHTML = '';
+                for (let node of nodeArray) {
+                    roughObj.roughEl.appendChild(<Node>node);
+                }
+            }
+        } else {
+            super.roughDrawOne(roughObj);
         }
+    }
 
-        .checkbox-input {
-          width: 0;
-          height: 0;
-          opacity: 0;
-          position: absolute;
-        }
+    static get styles() {
+        return [
+            super.styles,
+            css`
+              .slot {
+                display: inline-block;
+                vertical-align: middle;
+              }
 
-        .checkbox-wrapper {
-          margin: 0.25em 0 0 0;
-          float: left;
-          display: inline-block;
-          overflow: hidden;
-          position: relative;
-          border: none;
-          background: none;
-          outline: none;
-          width: 1em;
-          height: 1em;
-          line-height: 1em;
-          vertical-align: middle;
-        }
+              .checkbox {
+                overflow: hidden;
+                position: relative;
+                border: none;
+                background: none;
+                cursor: pointer;
+                outline: none;
+                height: 100%;
+              }
 
-        .checkbox-tick {
-          height: 100%;
-          width: 100%;
-          position: relative;
-        }
+              .checkbox-input {
+                width: 0;
+                height: 0;
+                opacity: 0;
+                position: absolute;
+              }
 
-        .checkbox[disabled] {
-          opacity: 0.5;
-          background: rgba(0, 0, 0, 0.03);
-          cursor: not-allowed;
-        }
-      `
-    ];
-  }
+              .checkbox-wrapper {
+                display: inline-block;
+                overflow: hidden;
+                position: relative;
+                border: none;
+                background: none;
+                outline: none;
+                width: 1em;
+                height: 1em;
+                line-height: 1em;
+                vertical-align: middle;
+              }
+
+              .checkbox-tick {
+                height: 100%;
+                width: 100%;
+                position: relative;
+              }
+
+              .checkbox[disabled] {
+                opacity: 0.5;
+                cursor: not-allowed;
+              }
+            `
+        ];
+    }
 
 
 }
--- src/component/hand-drawn-radio.ts
+++ src/component/hand-drawn-radio.ts
@@ -4,156 +4,152 @@ import {HandDrawnBase, RoughObjCanvas, RoughObjSvg} from './base/hand-drawn-base
 
 @customElement('hand-drawn-radio')
 export class HandDrawnRadio extends HandDrawnBase {
-  @property({type: Boolean, reflect: true}) disabled = false;
-  @property({type: Boolean}) checked = false;
-  @property({type: String}) value: string | null = null;
+    @property({type: Boolean, reflect: true}) disabled = false;
+    @property({type: Boolean}) checked = false;
+    @property({type: String}) value: string | null = null;
 
-  constructor() {
-    super();
-    this.attachShadow({mode: 'open'});
-  }
-
-  protected render() {
-    return html`
-        <label class="radio" ?disabled="${this.disabled}">
-            <!-- note: using type="checkbox" to focus on unchecked node by Tab   -->
-            <input class="radio-input" @change="${this.checkSwitchHandler}" type="checkbox" name="aa" ?disabled="${this.disabled}" .checked="${this.checked}" value="${this.value}">
-            <div id="dotWrapper" class="radio-wrapper rough">
-                <div id="dot" style=${this.checked ? 'display:inline-block' : 'display:none'} class="radio-dot rough"></div>
-            </div>
-            <slot class="slot" @slotchange="${this.roughRender}"></slot>
-        </label>
-    `;
-  }
-
-  protected createRenderRoot(): ShadowRoot | Element {
-    return super.createRenderRoot();
-  }
-
-  private checkSwitchHandler() {
-    this.checked = true;
-    this.dispatchEvent(new CustomEvent('change', {
-      composed: true,
-      bubbles: true,
-      detail: {
-        value: this.value,
-        checked: this.checked
-      }
-    }));
-  }
-
-  protected updateAnimationState() {
-    if (!this.disabled) {
-      super.updateAnimationState();
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
     }
-  }
 
-  protected roughDrawOne(roughObj: RoughObjSvg | RoughObjCanvas) {
-
-    const size = {
-      width: roughObj.roughParentEl.clientWidth,
-      height: roughObj.roughParentEl.clientHeight
-    };
-    if (roughObj.roughParentEl.id === 'dot') {
-      if (roughObj.roughEl instanceof HTMLCanvasElement) {
-        roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
-      }
-      const nodeArray = [];
-      nodeArray.push(roughObj.roughInstance.circle(size.width / 2, size.height / 2, (size.width - this.roughPadding*1.5) / 2,
-        {
-          ...this.roughOps,
-          roughness: (this.roughOps.roughness || 0) / 3,
-          bowing: (this.roughOps.bowing || 0) / 3,
-          fill: this.roughOps.stroke,
-          strokeWidth: this.roughOpsOrigin.strokeWidth,
-          fillStyle: 'solid'
-        }
-      ));
-      if (roughObj.roughEl instanceof SVGSVGElement) {
-        roughObj.roughEl.innerHTML = '';
-        for (let node of nodeArray) {
-          roughObj.roughEl.appendChild(<Node>node);
-        }
-      }
-    } else if (roughObj.roughParentEl.id === 'dotWrapper') {
-      if (roughObj.roughEl instanceof HTMLCanvasElement) {
-        roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
-      }
-      const nodeArray = [];
-      nodeArray.push(roughObj.roughInstance.circle(size.width / 2, size.height / 2, size.width - this.roughPadding*1.2,
-        {
-          ...this.roughOps,
-          roughness: (this.roughOps.roughness || 0) * 2 / 3,
-          bowing: (this.roughOps.bowing || 0) * 2 / 3
-        }
-      ));
-      if (roughObj.roughEl instanceof SVGSVGElement) {
-        roughObj.roughEl.innerHTML = '';
-        for (let node of nodeArray) {
-          roughObj.roughEl.appendChild(<Node>node);
-        }
-      }
-    } else {
-      super.roughDrawOne(roughObj);
+    protected render() {
+        return html`
+            <label class="radio" ?disabled="${this.disabled}">
+                <!-- note: using type="checkbox" to focus on unchecked node by Tab   -->
+                <input class="radio-input" @change="${this.checkSwitchHandler}" type="checkbox" name="aa" ?disabled="${this.disabled}" .checked="${this.checked}" value="${this.value}">
+                <div id="dotWrapper" class="radio-wrapper rough">
+                    <div id="dot" style=${this.checked ? 'display:inline-block' : 'display:none'} class="radio-dot rough"></div>
+                </div><!--
+             --><slot class="slot" @slotchange="${this.roughRender}"></slot>
+            </label>
+        `;
     }
-  }
 
-  static get styles() {
-    return [
-      super.styles,
-      css`
-        .slot {
-          float: left;
-          display: inline-block;
-          vertical-align: middle;
-        }
+    protected createRenderRoot(): ShadowRoot | Element {
+        return super.createRenderRoot();
+    }
 
-        .radio {
-          overflow: hidden;
-          position: relative;
-          border: none;
-          background: none;
-          cursor: pointer;
-          outline: none;
-          height: 100%;
-        }
+    private checkSwitchHandler() {
+        this.checked = true;
+        this.dispatchEvent(new CustomEvent('change', {
+            composed: true,
+            bubbles: true,
+            detail: {
+                value: this.value,
+                checked: this.checked
+            }
+        }));
+    }
 
-        .radio-input {
-          width: 0;
-          height: 0;
-          opacity: 0;
-          position: absolute;
+    protected updateAnimationState() {
+        if (!this.disabled) {
+            super.updateAnimationState();
         }
+    }
 
-        .radio-wrapper {
-          margin: 0.25em 0 0 0;
-          float: left;
-          display: inline-block;
-          overflow: hidden;
-          position: relative;
-          border: none;
-          background: none;
-          outline: none;
-          width: 1em;
-          height: 1em;
-          line-height: 1em;
-          vertical-align: middle;
-        }
+    protected roughDrawOne(roughObj: RoughObjSvg | RoughObjCanvas) {
 
-        .radio-dot {
-          height: 100%;
-          width: 100%;
-          position: relative;
+        const size = {
+            width: roughObj.roughParentEl.clientWidth,
+            height: roughObj.roughParentEl.clientHeight
+        };
+        if (roughObj.roughParentEl.id === 'dot') {
+            if (roughObj.roughEl instanceof HTMLCanvasElement) {
+                roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
+            }
+            const nodeArray = [];
+            nodeArray.push(roughObj.roughInstance.circle(size.width / 2, size.height / 2, (size.width - this.roughPadding * 1.5) / 2,
+                {
+                    ...this.roughOps,
+                    roughness: (this.roughOps.roughness || 0) / 3,
+                    bowing: (this.roughOps.bowing || 0) / 3,
+                    fill: this.roughOps.stroke,
+                    strokeWidth: this.roughOpsOrigin.strokeWidth,
+                    fillStyle: 'solid'
+                }
+            ));
+            if (roughObj.roughEl instanceof SVGSVGElement) {
+                roughObj.roughEl.innerHTML = '';
+                for (let node of nodeArray) {
+                    roughObj.roughEl.appendChild(<Node>node);
+                }
+            }
+        } else if (roughObj.roughParentEl.id === 'dotWrapper') {
+            if (roughObj.roughEl instanceof HTMLCanvasElement) {
+                roughObj.roughEl.getContext('2d')?.clearRect(0, 0, this.clientWidth, this.clientHeight);
+            }
+            const nodeArray = [];
+            nodeArray.push(roughObj.roughInstance.circle(size.width / 2, size.height / 2, size.width - this.roughPadding * 1.2,
+                {
+                    ...this.roughOps,
+                    roughness: (this.roughOps.roughness || 0) * 2 / 3,
+                    bowing: (this.roughOps.bowing || 0) * 2 / 3
+                }
+            ));
+            if (roughObj.roughEl instanceof SVGSVGElement) {
+                roughObj.roughEl.innerHTML = '';
+                for (let node of nodeArray) {
+                    roughObj.roughEl.appendChild(<Node>node);
+                }
+            }
+        } else {
+            super.roughDrawOne(roughObj);
         }
+    }
 
-        .radio[disabled] {
-          opacity: 0.5;
-          background: rgba(0, 0, 0, 0.03);
-          cursor: not-allowed;
-        }
-      `
-    ];
-  }
+    static get styles() {
+        return [
+            super.styles,
+            css`
+              .slot {
+                display: inline-block;
+                vertical-align: middle;
+              }
+
+              .radio {
+                overflow: hidden;
+                position: relative;
+                border: none;
+                background: none;
+                cursor: pointer;
+                outline: none;
+                height: 100%;
+              }
+
+              .radio-input {
+                width: 0;
+                height: 0;
+                opacity: 0;
+                position: absolute;
+              }
+
+              .radio-wrapper {
+                display: inline-block;
+                overflow: hidden;
+                position: relative;
+                border: none;
+                background: none;
+                outline: none;
+                width: 1em;
+                height: 1em;
+                line-height: 1em;
+                vertical-align: middle;
+              }
+
+              .radio-dot {
+                height: 100%;
+                width: 100%;
+                position: relative;
+              }
+
+              .radio[disabled] {
+                opacity: 0.5;
+                cursor: not-allowed;
+              }
+            `
+        ];
+    }
 
 
 }
